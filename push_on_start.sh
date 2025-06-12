22adc7635b61e356b93fbb8ac49198ec014f81c5
Aibek Aldabergenov <aaldaber@gmail.com>
2025-06-13 00:51:30 +0500

    Revert "Update push_on_start.sh"
    This reverts commit 23a65104f4f42cb6a3d4701bca749946fee71daf.

--- push_on_start.sh
+++ push_on_start.sh
@@ -8,7 +8,7 @@ AUTH_HEADER=$(echo -n "$PUSHGATEWAY_USER:$PUSHGATEWAY_PASS" | base64)
 
 docker events --filter 'event=start' --format '{{.Time}} {{.Actor.Attributes.name}}' |
 while read timestamp container_name; do
-  echo "container_start_total{container=\"$container_name\"} 1" | \
+  echo "container_start{container=\"$container_name\"} 1" | \
   curl --data-binary @- \
        -H "Authorization: Basic $AUTH_HEADER" \
        "$PUSHGATEWAY_URL/metrics/job/container_start/instance/$container_name"
